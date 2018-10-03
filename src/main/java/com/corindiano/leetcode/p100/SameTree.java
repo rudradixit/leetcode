cbaa68bc2e914f7e4bcde0a008687b3f4d9c5725
Rudra A Dixit <rudradixit@gmail.com>
2018-10-03 13:29:00 -0400

    Solved 100

---
+++ src/main/java/com/corindiano/leetcode/p100/SameTree.java
@@ -0,0 +1,15 @@
+package com.corindiano.leetcode.p100;
+
+import com.corindiano.leetcode.common.TreeNode;
+
+class SameTree {
+    boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null) return false;
+        if (q == null) return false;
+
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
