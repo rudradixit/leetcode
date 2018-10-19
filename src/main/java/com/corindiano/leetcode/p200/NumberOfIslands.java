1723e77139885937a03b921f78a1207c1c395e42
Rudra A Dixit <rudradixit@gmail.com>
2018-10-19 15:38:51 -0400

    Updates to NumberOfIslands

--- src/main/java/com/corindiano/leetcode/p200/NumberOfIslands.java
+++ src/main/java/com/corindiano/leetcode/p200/NumberOfIslands.java
@@ -1,30 +1,28 @@
 package com.corindiano.leetcode.p200;
 
 class NumberOfIslands {
-    private static void _countIslands(char[][] grid, int r, int c, boolean[][] used) {
+    private static void _countIslands(char[][] grid, int r, int c) {
         if (r < 0 || r >= grid.length) return;
         if (c < 0 || c >= grid[r].length) return;
-        if (used[r][c]) return;
-        if (grid[r][c] == '0') return;
+        if (grid[r][c] != '1') return;
 
-        used[r][c] = true;
+        grid[r][c] = '*';
 
-        if (r > 0) _countIslands(grid, r-1, c, used);
-        if (r < grid.length - 1) _countIslands(grid, r+1, c, used);
-        if (c > 0) _countIslands(grid, r, c-1, used);
-        if (c < grid[r].length - 1) _countIslands(grid, r, c+1, used);
+        _countIslands(grid, r-1, c);
+        _countIslands(grid, r+1, c);
+        _countIslands(grid, r, c-1);
+        _countIslands(grid, r, c+1);
     }
 
     static int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) return 0;
 
-        boolean[][] used = new boolean[grid.length][grid[0].length];
         int islands = 0;
 
         for (int i=0; i<grid.length; i++)
             for (int j=0; j<grid[i].length; j++) {
-                if (grid[i][j] == '1' && !used[i][j]) {
-                    _countIslands(grid, i, j, used);
+                if (grid[i][j] == '1') {
+                    _countIslands(grid, i, j);
                     ++islands;
                 }
             }
